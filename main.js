7597054b7bae1bdaace1a192ef0a08fd246de65a
zsv <zsv@linuxwizard.ru>
2019-02-20 13:31:13 +0300

    fix

--- main.js
+++ main.js
@@ -8,20 +8,33 @@ function Pacient() {
 			return item.prof != doctor.prof;
 		});
 	};
+	this.giveMany = (sum) => {
+		this._doctors.forEach((doctor) => {
+			doctor.takeManyPacient(sum);
+		});
+	}
 }
 
 function Doctor(prof) {
 	this.prof = prof;
+	this.many = 0;
+	this.takeManyPacient = (sum) => {
+		this.many += sum;
+	};
 };
 
 const vasia = new Pacient();
 
 const herurg = new Doctor('herurg');
 const pediatr = new Doctor('pediatr');
-
+const terapeft = new Doctor('terapeft');
+const psyhiatr = new Doctor('psyhiatr');
 
 vasia.addDoctor(herurg);
 vasia.addDoctor(pediatr);
+vasia.addDoctor(terapeft);
+vasia.addDoctor(psyhiatr);
 console.log('vasia add', vasia);
 vasia.deleteDoctor(pediatr);
+vasia.giveMany(99);
 console.log('vasia delete', vasia);
